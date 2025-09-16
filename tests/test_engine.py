9c3d48b04f096b98956b30f6241b05b85be952c7
stayuplate <simonwegmueller@gmail.com>
2025-09-16 16:03:49 +0200

    Add component removal guard and test

---
+++ tests/test_engine.py
@@ -0,0 +1,20 @@
+from engine.entity import Component, Entity
+
+
+class DummyComponent(Component):
+    def __init__(self) -> None:
+        super().__init__()
+        self.removed_called = False
+
+    def on_removed_from_scene(self) -> None:
+        self.removed_called = True
+
+
+def test_remove_component_without_scene_does_not_call_hook() -> None:
+    entity = Entity()
+    component = DummyComponent()
+    entity.add_component(component)
+
+    entity.remove_component(DummyComponent)
+
+    assert not component.removed_called
